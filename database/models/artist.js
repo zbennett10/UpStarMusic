b0b1c1ba2632b1b56f2a30af44e2a1b5db28fd88
Zachary Bennett <zbennett10@hotmail.com>
2017-02-18 14:23:00 -0500

    created models and starting queries

--- database/models/artist.js
+++ database/models/artist.js
@@ -1,1 +1,19 @@
-// Todo: Create Artist Model
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+const AlbumSchema = require('./album');
+
+const ArtistSchema = new Schema({
+    name: String,
+    age: Number,
+    yearsActive: Number,
+    image: String,
+    genre: String,
+    website: String,
+    netWorth: Number,
+    labelName: String,
+    retired: Boolean,
+    albums: [AlbumSchema]
+});
+
+const Artist = mongoose.model('artist', ArtistSchema);
+module.exports = Artist;
